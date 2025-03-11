91893d28d2539dd82b349b76f3d986182be70a29
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-11 14:55:20 -0300

    header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css';
+import styles from './App.module.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      To Do React Ts
+      <Header/>
+      <main className={styles.main}>
+        <h1>Conteudo...</h1>
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS To Do</span> @ 2025
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
